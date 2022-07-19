810f582ab4255b31b7fee9bb7198cf36e0704a03
Zeeshan Haider <zhadier39@gmail.com>
2022-07-20 04:38:03 +0500

    Time: 63 ms (99.59%), Space: 43.5 MB (96.70%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    const val = s.match(/^\s*[-|+]?(?=\d)[0-9]*/);
+    const upperLimit = Math.pow(2,31)-1;
+    const lowerLimit = -Math.pow(2,31);
+    if(val == null) {
+        return 0;
+    }
+   const calc = parseInt(val[0]);
+    if(val> upperLimit) {
+        return upperLimit
+    }
+    else if(val< lowerLimit) {
+        return lowerLimit
+    }
+    return val;
+};
