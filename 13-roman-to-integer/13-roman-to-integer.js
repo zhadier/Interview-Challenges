09de55e2029be41c1f0deffbcac2419dc7d6f43a
Zeeshan Haider <zhadier39@gmail.com>
2022-07-23 01:04:25 +0500

    Time: 230 ms (31.01%), Space: 49.5 MB (14.29%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -4,15 +4,14 @@
  */
 var romanToInt = function(s) {
     let map = new Map([['I',1], ['V',5], ['X',10], ['L',50], ['C',100], ['D',500], ['M',1000]]);
-    let str = s.split('').map((a)=>map.get(a));
     let sum = 0;
     for(let i=0; i<s.length;i++) {
-        if(str[i]< str[i+1]) {
-            sum += str[i+1]-str[i];
+        if(map.get(s[i])< map.get(s[i+1])) {
+            sum += map.get(s[i+1])-map.get(s[i]);
             i++;
         }
         else {
-            sum+=str[i];
+            sum+=map.get(s[i]);
         }
     }
     return sum
