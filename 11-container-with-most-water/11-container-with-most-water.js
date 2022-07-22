641eeb3aeda89525aa90f4f270274b919d79cfbf
Zeeshan Haider <zhadier39@gmail.com>
2022-07-23 00:43:42 +0500

    Time: 111 ms (53.99%), Space: 52.5 MB (5.02%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let filtered = height.filter(height => height!==0);
+    let max =0;
+    let i =0;
+    let j = height.length -1;
+    let volume;
+    while (i < j) {
+        let currentW = j - i;
+        if(height[i] <= height[j]) {
+            volume = height[i] * currentW;
+            i++;
+        }    
+        else {
+             volume = height[j] * currentW;
+             j--;
+        }
+       
+        if(volume > max) {
+            max = volume;
+         }
+  }
+    return max;
+};
