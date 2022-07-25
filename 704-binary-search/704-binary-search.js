96c8b6b3f531bec0a39b53d9959d95c86bf91bc4
Zeeshan Haider <zhadier39@gmail.com>
2022-07-26 01:29:35 +0500

    Time: 129 ms (7.33%), Space: 45.3 MB (21.17%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,42 +5,14 @@
  */
     let count = 0;
 var search = function(nums, target) {
-    if(nums.length === 1) {
-        if(nums[0]===target) {
-        return 0;
-        }
-        else {
-         return -1;
-        }
+     let pivot, left = 0, right = nums.length - 1;
+     while (left <= right) {
+       pivot = Math.floor((left + (right - left) / 2));
+       console.log(pivot,left,right, nums[pivot])
+       if (nums[pivot] === target) return pivot;
+       if (target < nums[pivot]) right = pivot - 1;
+       else left = pivot + 1;
     }
-   return find(0, nums.length -1, nums, target)
+    return -1;
 };
     
-var find = (start, end, nums, target) => {
-        let midpoint;
-        if((end-start) <= 1) {
-            if(nums[start]===target) {
-                return start;
-            }
-            else if (nums[end]=== target) {
-                return end;
-            }
-            return -1;
-        }
-        if((end-start) % 2) {
-            midpoint = ((start+end) +1) / 2
-        }
-        else {
-            midpoint = (start+end) / 2
-        }
-        if(nums[midpoint] === target) {
-            return midpoint;
-        }
-        else if (nums[midpoint] > target) {
-            return find(start, midpoint-1, nums, target);
-        }
-        else {
-            count ++;
-             return find(midpoint+1, end, nums, target);
-        }
-    }
