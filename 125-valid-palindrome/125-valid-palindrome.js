b57e9a0fc69fbbcd2e00ac48dffaf1afc2ab2c73
Zeeshan Haider <zhadier39@gmail.com>
2022-07-25 20:25:51 +0500

    Time: 83 ms (84.61%), Space: 47.8 MB (28.47%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,7 +4,6 @@
  */
 var isPalindrome = function(s) {
     let check = s.match(/[a-z|0-9]/ig)
-    console.log(check);
     if(check === null || check.join('').toLowerCase() === check.reverse().join('').toLowerCase()) {
         return true
     }
