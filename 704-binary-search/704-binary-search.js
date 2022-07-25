b890f2f4805a02742d329fee0b977162ca59be23
Zeeshan Haider <zhadier39@gmail.com>
2022-07-26 01:23:12 +0500

    Time: 105 ms (33.90%), Space: 45.3 MB (21.17%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+    let count = 0;
+var search = function(nums, target) {
+    if(nums.length === 1) {
+        if(nums[0]===target) {
+        return 0;
+        }
+        else {
+         return -1;
+        }
+    }
+   return find(0, nums.length -1, nums, target)
+};
+    
+var find = (start, end, nums, target) => {
+        let midpoint;
+        if((end-start) <= 1) {
+            if(nums[start]===target) {
+                return start;
+            }
+            else if (nums[end]=== target) {
+                return end;
+            }
+            return -1;
+        }
+        if((end-start) % 2) {
+            midpoint = ((start+end) +1) / 2
+        }
+        else {
+            midpoint = (start+end) / 2
+        }
+        if(nums[midpoint] === target) {
+            return midpoint;
+        }
+        else if (nums[midpoint] > target) {
+            return find(start, midpoint, nums, target);
+        }
+        else {
+            count ++;
+             return find(midpoint, end, nums, target);
+        }
+    }
