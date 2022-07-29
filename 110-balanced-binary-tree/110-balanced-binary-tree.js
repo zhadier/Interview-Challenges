51e380ce59d843eacc060c19a19f74378b9b93b3
Zeeshan Haider <zhadier39@gmail.com>
2022-07-30 01:58:10 +0500

    Time: 304 ms (5.05%), Space: 50.6 MB (5.00%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    let height = 1;
+    if(!root || (root?.left === null && root?.right=== null)){
+        return height;
+    }
+    let a = 0, b = 0;
+    if(root.left !== null) {
+         a = isBalanced(root.left);
+    }
+    if(root.right !== null) {
+         b = isBalanced(root.right);
+    }
+
+    if((Math.abs(a-b)) > 1 || a === false || b === false ) {
+        return false;
+    }
+    height+= Math.max(a, b);
+    console.log(height, a, b);
+    return height;
+};
