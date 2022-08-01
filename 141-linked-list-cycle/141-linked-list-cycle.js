754428fb7420b79d14b0f14bc86abfc049df9249
Zeeshan Haider <zhadier39@gmail.com>
2022-08-02 01:05:36 +0500

    Time: 132 ms (19.68%), Space: 44.2 MB (97.88%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -11,18 +11,11 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(!head) {
-        return false;
-    }
-    let pointer = head;
-    const array = [];
-    while(pointer.next !==null) {
-        if(array.includes(pointer)) {
-                return true;
-        }
-        const temp = pointer;
-        array.push(temp);
-        pointer = pointer.next;
-    }
-    return false;
+  let fast = head;
+  while (fast && fast.next) {
+    head = head.next;
+    fast = fast.next.next;
+    if (head === fast) return true;
+  }
+  return false;
 };
